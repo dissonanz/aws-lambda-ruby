7982fcbfd6ff3c70408933d810efcd00f78ebedf
Ilya Fomin <ifomin@aligntech.com>
2015-10-10 17:34:21 -0700

    Fix for library directory

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ print('Loading function')
 
 
 def lambda_handler(event, context):
-  cmd = u'LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH ./bin/ruby -v 2>&1'
+  cmd = u'LD_LIBRARY_PATH=./bin:$LD_LIBRARY_PATH ./bin/ruby -v 2>&1'
   message = os.popen(cmd).read()
   print message
   return
