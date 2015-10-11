90e0df7446a85e8a23601d97b6a3f489b0d3eff8
Ilya Fomin <ifomin@aligntech.com>
2015-10-10 17:12:20 -0700

    Restructured the repo

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo -e "Creating lambda.zip\n"
-zip lambda.zip ruby libruby.so.2.0 *.rb *.py
+mkdir out
+zip out/lambda.zip bin/* *.rb *.py
 if [ $? -eq 0 ];
   then
   echo -e "\nZip created. Upload lambda.zip to AWS Lambda as a python function in the normal way."
