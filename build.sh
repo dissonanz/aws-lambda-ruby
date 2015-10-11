e66293e002d583aac427bf008a357f4892f8cf57
Ilya Fomin <ifomin@aligntech.com>
2015-10-10 17:54:41 -0700

    Changing lambda function to run hello world

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm -rf out/*
 mkdir out
 
 echo -e "Creating lambda.zip\n"
-zip out/lambda.zip bin/* *.rb *.py
+zip out/lambda.zip bin/* src/* *.py
 if [ $? -eq 0 ];
   then
   echo -e "\nZip created. Upload lambda.zip to AWS Lambda as a python function in the normal way."
--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,9 @@
-import os
+from subprocess import Popen, PIPE, STDOUT
 print('Loading function')
 
-
 def lambda_handler(event, context):
-  cmd = u'LD_LIBRARY_PATH=./bin:$LD_LIBRARY_PATH ./bin/ruby -v 2>&1'
-  message = os.popen(cmd).read()
-  print message
+  cmd = u'LD_LIBRARY_PATH=./bin:$LD_LIBRARY_PATH ./bin/ruby ./src/action.rb'
+  p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+  output = p.stdout.read()
+  print output
   return
